8f577db4b2b91104de0a5b3aa8017edbd38ef10d
Shirley Yin <sshirley.yin@gmail.com>
2019-02-10 15:05:24 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import fastai
+from fastai.vision import *
+
+
+def main():
+    print('here')
+    train = 'train_images/'
+    labels = 'traininglabels.csv'
+    test = 'leaderboard_test_data/'
+    holdout = 'leaderboard_holdout_data/'
+    
+
+
+if __name__ == '__main__':
+    main()
